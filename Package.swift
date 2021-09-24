bb4f6a0fdedc2d98849d17ad4a6f56ec5e9e5612
Peter Szots <peter.szots@apps.no>
2021-09-24 09:10:31 +0200

    Add SPM support

--- Package.swift
+++ Package.swift
@@ -3,5 +3,18 @@ import PackageDescription
 
 let package = Package(
     name: "KVKCalendar",
-    platforms: [.iOS(.v13)]
+    platforms: [.iOS(.v13)],
+    products: [
+        .library(
+            name: "KVKCalendar",
+            targets: ["KVKCalendar"]),
+    ],
+    dependencies: [
+    ],
+    targets: [
+        .target(
+            name: "KVKCalendar",
+            dependencies: [],
+            path: "KVKCalendar"),
+    ]
 )
